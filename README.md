54e71ef7b165a0c700682c5ae54ad8617a206fbb
startdusk <522383345@qq.com>
2022-06-19 18:26:15 +0800

    init🎉:  init project

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # clipstash
+
+### Web service which allows the user to paste and share clipboard content (clips)
+
+### Features:
+
+- Link sharing
+- Clip expiration
+- JSON API
+- Password Protected cips
---
+++ migrations/clips.sql
@@ -0,0 +1,14 @@
+-- Add migration script here
+
+CREATE TABLE IF NOT EXISTS clips (
+    clip_id TEXT PRIMARY KEY NOT NULL,
+    shortcode TEXT UNIQUE NOT NULL,
+    content TEXT NOT NULL,
+    title TEXT,
+    posted TIMESTAMP NOT NULL,
+    expires TIMESTAMP,
+    password TEXT,
+    hits BIGINT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS api_keys (api_key BYTEA PRIMARY KEY);
